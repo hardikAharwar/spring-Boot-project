6ea7d80b0c1e1ef45861f6ae3c1be8bd2c0478be
Hardik <hardik.aharwar24@gmail.com>
2026-01-05 04:15:38 +0530

    Add files via upload

---
+++ springproject/java/JpaProject10Application.java
@@ -0,0 +1,14 @@
+package com.projectdemo.JpaProject10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaProject10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaProject10Application.class, args);
+	
+	System.out.println(" Used car project running");}
+
+}
